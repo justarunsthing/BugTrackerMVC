7c331927f082e3c4c729fa4d44cdc63aefee9650
justarunsthing <justarunsthing@outlook.com>
2025-06-29 16:33:14 +0100

    Added TicketPriority class

---
+++ Models/TicketPriority.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace BugTrackerMVC.Models
+{
+    public class TicketPriority
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ticket Priority")]
+        public string Name { get; set; }
+    }
+}
