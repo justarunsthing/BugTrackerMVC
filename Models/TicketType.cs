4fc45017eee6772358e27fd64f39d6313de41b5c
justarunsthing <justarunsthing@outlook.com>
2025-06-29 16:29:55 +0100

    Added TicketType class

---
+++ Models/TicketType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace BugTrackerMVC.Models
+{
+    public class TicketType
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ticket Type")]
+        public string Name { get; set; }
+    }
+}
