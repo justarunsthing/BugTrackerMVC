5ad43529fcec9722833d028f721ce0949120cb24
justarunsthing <justarunsthing@outlook.com>
2025-07-02 09:15:09 +0100

    Updated BTUser class

--- Models/BTUser.cs
+++ Models/BTUser.cs
@@ -17,5 +17,21 @@ namespace BugTrackerMVC.Models
         [NotMapped]
         [Display(Name = "Full Name")]
         public string FullName => $"{FirstName} {LastName}";
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile AvatarFormFile { get; set; }
+
+        [Display(Name = "Avatar")]
+        public string AvatarFileName { get; set; }
+        public byte[] AvatarFileData { get; set; }
+
+        [Display(Name = "File Extension")]
+        public string AvatarContentType { get; set; }
+        public int? CompanyId { get; set; }
+
+        // Navigation properties
+        public virtual Company Company { get; set; }
+        public virtual ICollection<Project> Projects { get; set; }
     }
 }
