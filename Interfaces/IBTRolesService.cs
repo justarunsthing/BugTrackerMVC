b831869f1ee65aa7978a17f1f482fc92f9fe77f3
justarunsthing <justarunsthing@outlook.com>
2025-07-05 12:11:14 +0100

    Added definition for IBTRolesService

---
+++ Interfaces/IBTRolesService.cs
@@ -0,0 +1,16 @@
+﻿using BugTrackerMVC.Models;
+
+namespace BugTrackerMVC.Interfaces
+{
+    public interface IBTRolesService
+    {
+        Task<bool> IsUserInRoleAsync(BTUser user, string roleName);
+        Task<IEnumerable<string>> GetUserRolesAsync(BTUser user);
+        Task<bool> AddUserToRoleAsync(BTUser user, string roleName);
+        Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName);
+        Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles);
+        Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId);
+        Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId);
+        Task<string> GetRoleNameByIdAsync(string roleId);
+    }
+}
