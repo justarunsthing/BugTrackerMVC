5028cf53cfdebca65e662dec7c60a85ce0d0d803
justarunsthing <justarunsthing@outlook.com>
2025-07-06 13:37:15 +0100

    Added BTCompanyInfoService class

---
+++ Services/BTCompanyInfoService.cs
@@ -0,0 +1,28 @@
+﻿using BugTrackerMVC.Models;
+using BugTrackerMVC.Interfaces;
+
+namespace BugTrackerMVC.Services
+{
+    public class BTCompanyInfoService : IBTCompanyInfoService
+    {
+        public Task<List<BTUser>> GetAllMembersAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Project>> GetAllProjectsAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketsAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Company> GetCompanyInfoByIdAsync(int? companyId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
