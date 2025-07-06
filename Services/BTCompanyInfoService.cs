aab2642669d35aba1931efa3029193b71f3c84eb
justarunsthing <justarunsthing@outlook.com>
2025-07-06 13:39:05 +0100

    Inject ApplicationDbContext into BTCompanyInfoService

--- Services/BTCompanyInfoService.cs
+++ Services/BTCompanyInfoService.cs
@@ -1,10 +1,18 @@
 ﻿using BugTrackerMVC.Models;
 using BugTrackerMVC.Interfaces;
+using BugTrackerMVC.Data;
 
 namespace BugTrackerMVC.Services
 {
     public class BTCompanyInfoService : IBTCompanyInfoService
     {
+        private readonly ApplicationDbContext _context;
+
+        public BTCompanyInfoService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public Task<List<BTUser>> GetAllMembersAsync(int companyId)
         {
             throw new NotImplementedException();
