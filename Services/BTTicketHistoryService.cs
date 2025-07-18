93052092b971af49c4bd69664747730ae94987ee
justarunsthing <justarunsthing@outlook.com>
2025-07-18 08:34:48 +0100

    Added BTTicketHistoryService class

---
+++ Services/BTTicketHistoryService.cs
@@ -0,0 +1,23 @@
+﻿using BugTrackerMVC.Models;
+using BugTrackerMVC.Interfaces;
+
+namespace BugTrackerMVC.Services
+{
+    public class BTTicketHistoryService : IBTTicketHistoryService
+    {
+        public Task AddHistoryAsync(Ticket oldTicket, Ticket newTicket, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TicketHistory>> GetCompanyTicketHistoriesAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TicketHistory>> GetProjectTicketsHistoriesAsync(int projectId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
