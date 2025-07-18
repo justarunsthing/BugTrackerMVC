aee67b784272923550ea7984077419234ac762b6
justarunsthing <justarunsthing@outlook.com>
2025-07-18 08:35:41 +0100

    Inject ApplicationDbContext into BTTicketHistoryService

--- Services/BTTicketHistoryService.cs
+++ Services/BTTicketHistoryService.cs
@@ -1,10 +1,18 @@
-﻿using BugTrackerMVC.Models;
+﻿using BugTrackerMVC.Data;
+using BugTrackerMVC.Models;
 using BugTrackerMVC.Interfaces;
 
 namespace BugTrackerMVC.Services
 {
     public class BTTicketHistoryService : IBTTicketHistoryService
     {
+        private readonly ApplicationDbContext _context;
+
+        public BTTicketHistoryService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public Task AddHistoryAsync(Ticket oldTicket, Ticket newTicket, string userId)
         {
             throw new NotImplementedException();
