bb299b1f16e471266244c928d6b66dad0b764119
justarunsthing <justarunsthing@outlook.com>
2025-07-19 20:37:54 +0100

    Added BTEmailService class

---
+++ Services/BTEmailService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace BugTrackerMVC.Services
+{
+    public class BTEmailService : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
