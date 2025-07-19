0144a0290382f8d9a261396503d0b0dd74db4a32
justarunsthing <justarunsthing@outlook.com>
2025-07-19 20:40:56 +0100

    Inject MailSettings into BTEmailService

--- Services/BTEmailService.cs
+++ Services/BTEmailService.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using BugTrackerMVC.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace BugTrackerMVC.Services
 {
     public class BTEmailService : IEmailSender
     {
+        private readonly MailSettings _mailSettings;
+
+        public BTEmailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             throw new NotImplementedException();
