e48a866fb59dd7f230b2f0742f785e77878fd240
justarunsthing <justarunsthing@outlook.com>
2025-07-20 16:29:29 +0100

    Implemented SendEmailAsync method

--- Services/BTEmailService.cs
+++ Services/BTEmailService.cs
@@ -1,4 +1,7 @@
-﻿using BugTrackerMVC.Models;
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using BugTrackerMVC.Models;
 using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Identity.UI.Services;
 
@@ -13,9 +16,33 @@ namespace BugTrackerMVC.Services
             _mailSettings = mailSettings.Value;
         }
 
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(emailTo));
+            email.Subject = subject;
+
+            var builder = new BodyBuilder
+            {
+                HtmlBody = htmlMessage
+            };
+
+            email.Body = builder.ToMessageBody();
+
+            try
+            {
+                using var smtp = new SmtpClient();
+                smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+                smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+
+                await smtp.SendAsync(email);
+                smtp.Disconnect(true);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
