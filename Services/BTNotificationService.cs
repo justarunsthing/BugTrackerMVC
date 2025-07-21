759744ae67aca4862d4b1d1529f950bbca67d5d0
justarunsthing <justarunsthing@outlook.com>
2025-07-21 08:43:23 +0100

    Added BTNotificationService class

---
+++ Services/BTNotificationService.cs
@@ -0,0 +1,38 @@
+﻿using BugTrackerMVC.Models;
+using BugTrackerMVC.Interfaces;
+
+namespace BugTrackerMVC.Services
+{
+    public class BTNotificationService : IBTNotificationService
+    {
+        public Task AddNotificationAsync(Notification notification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Notification>> GetReceivedNotificationsAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Notification>> GetSentNotificationsAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendEmailNotificationAsync(Notification notification, string emailSubject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendEmailNotificationsByRoleAsync(Notification notification, int companyId, string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendMembersEmailNotificationsAsync(Notification notification, List<BTUser> members)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
