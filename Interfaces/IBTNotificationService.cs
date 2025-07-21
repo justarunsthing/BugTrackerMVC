a706455f4d55080b02b474c16cdb56522bd6063d
justarunsthing <justarunsthing@outlook.com>
2025-07-21 08:42:01 +0100

    Added IBTNotificationService interface

---
+++ Interfaces/IBTNotificationService.cs
@@ -0,0 +1,14 @@
+﻿using BugTrackerMVC.Models;
+
+namespace BugTrackerMVC.Interfaces
+{
+    public interface IBTNotificationService
+    {
+        Task AddNotificationAsync(Notification notification);
+        Task<List<Notification>> GetReceivedNotificationsAsync(string userId);
+        Task<List<Notification>> GetSentNotificationsAsync(string userId);
+        Task SendEmailNotificationsByRoleAsync(Notification notification, int companyId, string role);
+        Task SendMembersEmailNotificationsAsync(Notification notification, List<BTUser> members);
+        Task SendEmailNotificationAsync(Notification notification, string emailSubject);
+    }
+}
