4c95ad9cc4b2d8c3e45b44b92c05e3761bfb7399
justarunsthing <justarunsthing@outlook.com>
2025-07-23 08:46:36 +0100

    Added BTInviteService class

---
+++ Services/BTInviteService.cs
@@ -0,0 +1,38 @@
+﻿using BugTrackerMVC.Models;
+using BugTrackerMVC.Interfaces;
+
+namespace BugTrackerMVC.Services
+{
+    public class BTInviteService : IBTInviteService
+    {
+        public Task<bool> AcceptInviteAsync(Guid? token, string userId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddNewInviteAsync(Invite invite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AnyInviteAsync(Guid token, string email, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Invite> GetInviteAsync(int inviteId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Invite> GetInviteAsync(Guid token, string email, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ValidateInviteCodeAsync(Guid? token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
