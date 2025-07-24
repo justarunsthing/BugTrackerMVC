1e3b33d18b3ce9d03d88af3cf9f438ab9f5a54c7
justarunsthing <justarunsthing@outlook.com>
2025-07-24 08:45:47 +0100

    Added IBTFileService interface

---
+++ Interfaces/IBTFileService.cs
@@ -0,0 +1,10 @@
+﻿namespace BugTrackerMVC.Interfaces
+{
+    public interface IBTFileService
+    {
+        Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+        string ConvertByteArrayToFile(byte[] fileData, string extension);
+        string GetFileIcon(string file);
+        string FormatFileSize(long bytes);
+    }
+}
