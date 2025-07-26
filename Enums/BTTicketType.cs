ec33a6d644de995c3859bd793018ed7d058ba27f
justarunsthing <justarunsthing@outlook.com>
2025-07-26 21:02:04 +0100

    Added BTTicketType enum

---
+++ Enums/BTTicketType.cs
@@ -0,0 +1,12 @@
+﻿namespace BugTrackerMVC.Enums
+{
+    public enum BTTicketType
+    {
+        NewDevelopment,
+        WorkTask,
+        Defect,
+        ChangeRequest,
+        Enhancement,
+        GeneralTask
+    }
+}
