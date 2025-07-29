0c7aa28e9a325eac571e267736b16c9261a0acee
justarunsthing <justarunsthing@outlook.com>
2025-07-29 08:47:27 +0100

    Added BTUserClaimsPrincipalFactory class

---
+++ Services/Factories/BTUserClaimsPrincipalFactory.cs
@@ -0,0 +1,17 @@
+﻿using BugTrackerMVC.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Identity;
+
+namespace BugTrackerMVC.Services.Factories
+{
+    public class BTUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<BTUser, IdentityRole>
+    {
+        public BTUserClaimsPrincipalFactory(UserManager<BTUser> userManager, 
+                                            RoleManager<IdentityRole> roleManager,
+                                            IOptions<IdentityOptions> optionsAccessor)
+        : base(userManager, roleManager, optionsAccessor)
+        {
+   
+        }
+    }
+}
