b7fe70fb714dd21e064a2747d84fb8d6efefa7ef
justarunsthing <justarunsthing@outlook.com>
2025-07-30 08:29:45 +0100

    Added UserRolesController

---
+++ Controllers/UserRolesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BugTrackerMVC.Controllers
+{
+    public class UserRolesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
