c6bbcf821b5bb1107d9a128e2183c9d9e8a7aa03
justarunsthing <justarunsthing@outlook.com>
2025-07-30 08:54:27 +0100

    Implemented ManageUserRoles controller method

--- Controllers/UserRolesController.cs
+++ Controllers/UserRolesController.cs
@@ -1,5 +1,9 @@
-﻿using BugTrackerMVC.Interfaces;
+﻿using BugTrackerMVC.Extensions;
+using BugTrackerMVC.Interfaces;
+using BugTrackerMVC.Models;
+using BugTrackerMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BugTrackerMVC.Controllers
 {
@@ -14,9 +18,27 @@ namespace BugTrackerMVC.Controllers
             _companyInfoService = companyInfoService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> ManageUserRoles()
         {
-            return View();
+            List<ManageUserRolesViewModel> model = [];
+            int companyId = User.Identity.GetCompanyId().Value;
+            List<BTUser> users = await _companyInfoService.GetAllMembersAsync(companyId);
+
+            foreach (var user in users)
+            {
+                ManageUserRolesViewModel viewModel = new()
+                {
+                    BTUser = user
+                };
+
+                var selectedRoles = await _rolesService.GetUserRolesAsync(user);
+                var roles = await _rolesService.GetRolesAsync();
+                viewModel.Roles = new MultiSelectList(roles, "Name", "Name", selectedRoles);
+
+                model.Add(viewModel);
+            }
+
+            return View(model);
         }
     }
 }
