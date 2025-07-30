ca7e0bdf578ade0438c0fa6635b50892c097cab2
justarunsthing <justarunsthing@outlook.com>
2025-07-30 08:39:02 +0100

    Inject dependencies to UserRolesController

--- Controllers/UserRolesController.cs
+++ Controllers/UserRolesController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BugTrackerMVC.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BugTrackerMVC.Controllers
 {
     public class UserRolesController : Controller
     {
+        private readonly IBTRolesService _rolesService;
+        private readonly IBTCompanyInfoService _companyInfoService;
+
+        public UserRolesController(IBTRolesService rolesService, IBTCompanyInfoService companyInfoService)
+        {
+            _rolesService = rolesService;
+            _companyInfoService = companyInfoService;
+        }
+
         public IActionResult Index()
         {
             return View();
