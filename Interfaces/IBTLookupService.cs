3c43d3c056f521c9e074e0b54d90942add533075
justarunsthing <justarunsthing@outlook.com>
2025-08-01 09:01:01 +0100

    Added IBTLookupService interface

---
+++ Interfaces/IBTLookupService.cs
@@ -0,0 +1,12 @@
+﻿using BugTrackerMVC.Models;
+
+namespace BugTrackerMVC.Interfaces
+{
+    public interface IBTLookupService
+    {
+        Task<List<TicketPriority>> GetTicketPrioritiesAsync();
+        Task<List<TicketStatus>> GetTicketStatusesAsync();
+        Task<List<TicketType>> GetTicketTypesAsync();
+        Task<List<ProjectPriority>> GetProjectPrioritiesAsync();
+    }
+}
