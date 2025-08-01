fae9dccaf1503e1e364fb6e80314059adc499253
justarunsthing <justarunsthing@outlook.com>
2025-08-01 09:01:58 +0100

    Added BTLookupService class & implement interface

---
+++ Services/BTLookupService.cs
@@ -0,0 +1,28 @@
+﻿using BugTrackerMVC.Models;
+using BugTrackerMVC.Interfaces;
+
+namespace BugTrackerMVC.Services
+{
+    public class BTLookupService : IBTLookupService
+    {
+        public Task<List<ProjectPriority>> GetProjectPrioritiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TicketPriority>> GetTicketPrioritiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TicketStatus>> GetTicketStatusesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TicketType>> GetTicketTypesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
