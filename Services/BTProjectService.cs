abcd6d7a4bfcd9dfa05fe047f02ca488ce76a3ca
justarunsthing <justarunsthing@outlook.com>
2025-08-09 18:21:39 +0100

    Updated GetProjectByIdAsync method

--- Services/BTProjectService.cs
+++ Services/BTProjectService.cs
@@ -209,6 +209,15 @@ namespace BugTrackerMVC.Services
         {
             var project = await _context.Projects
                                         .Include(p => p.Tickets)
+                                            .ThenInclude(t => t.TicketPriority)
+                                        .Include(p => p.Tickets)
+                                            .ThenInclude(t => t.TicketStatus)
+                                        .Include(p => p.Tickets)
+                                            .ThenInclude(t => t.TicketType)
+                                        .Include(p => p.Tickets)
+                                            .ThenInclude(t => t.DeveloperUser)
+                                        .Include(p => p.Tickets)
+                                            .ThenInclude(t => t.OwnerUser)
                                         .Include(p => p.Members)
                                         .Include(p => p.ProjectPriority)
                                         .FirstOrDefaultAsync(p => p.Id == projectId && p.CompanyId == companyId);
