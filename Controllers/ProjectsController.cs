56b64208c2cbd0b60f9a3e8d359ed92e9c4e3270
justarunsthing <justarunsthing@outlook.com>
2025-08-09 18:26:45 +0100

    Updated Details method

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -90,10 +90,9 @@ namespace BugTrackerMVC.Controllers
                 return NotFound();
             }
 
-            var project = await _context.Projects
-                .Include(p => p.Company)
-                .Include(p => p.ProjectPriority)
-                .FirstOrDefaultAsync(m => m.Id == id);
+            int companyId = User.Identity.GetCompanyId().Value;
+            var project = await _projectService.GetProjectByIdAsync(id.Value, companyId);
+
             if (project == null)
             {
                 return NotFound();
