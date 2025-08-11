f98528d592c5b61ceb353f0b7be045a9e1d3f90b
justarunsthing <justarunsthing@outlook.com>
2025-08-11 08:49:23 +0100

    Implemented GetTicketPrioritiesAsync method

--- Services/BTLookupService.cs
+++ Services/BTLookupService.cs
@@ -26,9 +26,16 @@ namespace BugTrackerMVC.Services
             }
         }
 
-        public Task<List<TicketPriority>> GetTicketPrioritiesAsync()
+        public async Task<List<TicketPriority>> GetTicketPrioritiesAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return await _context.TicketPriorities.ToListAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public Task<List<TicketStatus>> GetTicketStatusesAsync()
