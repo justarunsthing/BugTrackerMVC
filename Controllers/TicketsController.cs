30772e414c14c5f4eec3e0dc3effeca459a88a01
justarunsthing <justarunsthing@outlook.com>
2025-08-15 08:40:49 +0100

    Added AllTickets controller action

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -50,6 +50,21 @@ namespace BugTrackerMVC.Controllers
             return View(tickets);
         }
 
+        public async Task<IActionResult> AllTickets()
+        {
+            int companyId = User.Identity.GetCompanyId().Value;
+            var tickets = await _ticketService.GetAllTicketsByCompanyAsync(companyId);
+
+            if (User.IsInRole(nameof(Roles.Developer)) || User.IsInRole(nameof(Roles.Submitter)))
+            {
+                return View(tickets.Where(t => t.IsArchived == false));
+            }
+            else
+            {
+                return View(tickets);
+            }
+        }
+
         // GET: Tickets/Details/5
         public async Task<IActionResult> Details(int? id)
         {
