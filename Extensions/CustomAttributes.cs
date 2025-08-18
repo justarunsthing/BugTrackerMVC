4cea95849c042a441a4a9b5e71f815eae3dafdf4
justarunsthing <justarunsthing@outlook.com>
2025-08-18 08:34:21 +0100

    Added AllowedExtensionsAttribute

--- Extensions/CustomAttributes.cs
+++ Extensions/CustomAttributes.cs
@@ -2,18 +2,51 @@
 
 namespace BugTrackerMVC.Extensions
 {
+    public class AllowedExtensionsAttribute : ValidationAttribute
+    {
+        private readonly string[] _extensions;
+
+        public AllowedExtensionsAttribute(string[] extensions)
+        {
+            _extensions = extensions;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+
+            if (file != null)
+            {
+                var extension = Path.GetExtension(file.FileName);
+                if (!_extensions.Contains(extension.ToLower()))
+                {
+                    return new ValidationResult(GetErrorMessage(extension));
+                }
+            }
+
+
+            return ValidationResult.Success;
+        }
+
+        public string GetErrorMessage(string ext)
+        {
+            return $"The file extension {ext} is not allowed!";
+        }
+    }
+
     public class MaxFileSizeAttribute : ValidationAttribute
     {
         private readonly int _maxFileSize;
+
         public MaxFileSizeAttribute(int maxFileSize)
         {
             _maxFileSize = maxFileSize;
         }
 
-        protected override ValidationResult IsValid(
-        object value, ValidationContext validationContext)
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             var file = value as IFormFile;
+
             if (file != null)
             {
                 if (file.Length > _maxFileSize)
