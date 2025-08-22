fd0121653e7650e419d15c47b8793275cc6fd290
justarunsthing <justarunsthing@outlook.com>
2025-08-22 08:45:58 +0100

    Added UnassignedProjects controller action

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -82,6 +82,14 @@ namespace BugTrackerMVC.Controllers
             return View(projects);
         }
 
+        public async Task<IActionResult> UnassignedProjects()
+        {
+            int companyId = User.Identity.GetCompanyId().Value;
+            var projects = await _projectService.GetUnassignedProjectsAsync(companyId);
+
+            return View(projects);
+        }
+
         // GET: Projects/Details/5
         public async Task<IActionResult> Details(int? id)
         {
