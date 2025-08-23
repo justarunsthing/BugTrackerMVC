1ee3caa63343d14383ba50b8e29bac50933bd867
justarunsthing <justarunsthing@outlook.com>
2025-08-23 20:16:05 +0100

    Added AssignPMViewModel model

---
+++ ViewModels/AssignPMViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BugTrackerMVC.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BugTrackerMVC.ViewModels
+{
+    public class AssignPMViewModel
+    {
+        public Project Project { get; set; }
+        public SelectList PMList { get; set; }
+        public string PMId { get; set; }
+    }
+}
