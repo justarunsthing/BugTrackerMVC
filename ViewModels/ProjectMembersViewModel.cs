0caf1012e40e7e49072a139f1a335e6853982a9e
justarunsthing <justarunsthing@outlook.com>
2025-08-24 20:26:01 +0100

    Added ProjectMembersViewModel

---
+++ ViewModels/ProjectMembersViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BugTrackerMVC.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BugTrackerMVC.ViewModels
+{
+    public class ProjectMembersViewModel
+    {
+        public Project Project { get; set; }
+        public MultiSelectList Users { get; set; }
+        public List<string> SelectedUsers { get; set; }
+    }
+}
