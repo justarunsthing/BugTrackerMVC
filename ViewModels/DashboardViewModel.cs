674e87220742af5a89d8ef6a3608f18a31da2951
justarunsthing <justarunsthing@outlook.com>
2025-09-03 08:35:48 +0100

    Added DashboardViewModel

---
+++ ViewModels/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BugTrackerMVC.Models;
+
+namespace BugTrackerMVC.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public Company Company { get; set; }
+        public List<Project> Projects { get; set; }
+        public List<Ticket> Tickets { get; set; }
+        public List<BTUser> Members { get; set; }
+    }
+}
